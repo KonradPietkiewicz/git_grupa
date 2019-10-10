ae63d464e808e205764c84076bcb433b54653f08
AndrzejAkrobata <wenrobert@wp.pl>
2019-10-10 19:25:07 +0200

    Added download, counter and exit methods

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
+import requests
+import os
+
+URL_FILE = 'https://s3.zylowski.net/public/input/2aa1.txt'
+
+def download_txt():
+    r = requests.get(URL_FILE)
+    if r.status_code == 200:
+        with open('file.txt', 'wb') as f:
+            f.write(r.content)
+        return 'Sukces! Pomyślnie pobrano plik.'
+    else:
+        return 'Nie ma plik .txt w takim URL: {}'.format(URL_FILE)
+
+
+def count_words():
+    with open('file.txt', 'r') as f:
+        counter = 0
+        for word in f.read().split():
+            counter += 1
+    return counter
+
+def exit():
+
+    if os.path.exists("file.txt"):
+        os.remove("file.txt")
+    if os.path.exists("statystyki.txt"):
+        os.remove("statystyki.txt")
+
+
 while True:
     print("""
     1. Pobierz plik z internetu
@@ -14,10 +44,12 @@ while True:
 
     if prompt == '1':
         print("1. Pobierz plik z internetu")
+        download_txt()
     elif prompt == '2':
         print('2. Zlicz liczbę liter w pobranym pliku')
     elif prompt == '3':
         print('3. Zlicz liczbę wyrazów w pliku')
+        count_words()
     elif prompt == '4':
         print('4. Zlicz liczbę znaków interpunkcyjnych w pliku')
     elif prompt == '5':
@@ -27,6 +59,7 @@ while True:
     elif prompt == '7':
         print('7. Zapisz statystyki z punktów 2-5 do pliku statystyki.txt')
     elif prompt == '8':
+        exit()
         break
     else:
         print('Niepoprawna wartość. Wybierz opcje 1-8.')
